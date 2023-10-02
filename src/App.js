1e81bda46e323e430be70bd9aa2d96beb9251e14
Dameli <112216121+dameli555@users.noreply.github.com>
2023-10-02 18:12:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/price" element={<Pricing />} />
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contacting />} />
